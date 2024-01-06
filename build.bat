0662ef32d8194654c6c48459ffb63f4213e9b90e
Manoel Schürmeier <github@manoel-schuermeier.de>
2024-01-06 17:50:18 +0100

    adjusted build script to not include debug_secrets.json

--- build.bat
+++ build.bat
@@ -3,8 +3,12 @@ set folder="build_output"
 if exist %folder% rmdir /s /q %folder%
 mkdir %folder%
 
+move assets\debug_secrets.json .
+
 :: Windows
 echo Building Windows
 call flutter build windows
 if not exist %folder%\windows mkdir %folder%\windows
-move build\windows\x64\runner\Release %folder%\windows
+move build\windows\x64\runner\Release\* %folder%\windows
+
+move debug_secrets.json assets
