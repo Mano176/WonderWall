ef4e9826da00145c21b4027e3788008b770258b8
Manoel Schürmeier <github@manoel-schuermeier.de>
2024-01-06 17:59:10 +0100

    fixed bug in buildscript: folders are now correctly included

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ move assets\debug_secrets.json .
 :: Windows
 echo Building Windows
 call flutter build windows
-if not exist %folder%\windows mkdir %folder%\windows
-move build\windows\x64\runner\Release\* %folder%\windows
+move build\windows\x64\runner\Release %folder%
+rename %folder%\Release windows
 
 move debug_secrets.json assets
