c971248b177c1784cb9ded3b441aa31bc93f1bd2
Manoel Schürmeier <github@manoel-schuermeier.de>
2026-02-14 01:32:06 +0100

    added automatic database migration

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,45 +1,144 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using DbContext = WonderWall.Database.DbContext;
 
-namespace WonderWall
+namespace WonderWall;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    private bool IsInitialized;
+
+    public App()
     {
-        private bool IsInitialized;
+        InitializeComponent();
+        CheckDbMigration();
+    }
 
-        public App()
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        Initialize();
+
+        Window window = new Window(new AppShell());
+        window.Title = "WonderWall";
+        window.MinimumWidth = 1200;
+        window.MinimumHeight = 900;
+
+        return window;
+    }
+
+    private void Initialize()
+    {
+        if (IsInitialized)
+            return;
+
+        IsInitialized = true;
+        InitializeTrayIcon();
+    }
+
+    private void InitializeTrayIcon()
+    {
+        ITrayService trayService = ServiceProvider.GetService<ITrayService>();
+
+        if (trayService != null)
         {
-            InitializeComponent();
+            trayService.Initialize();
+        }
+    }
+
+    private void CheckDbMigration()
+    {
+        string dbGenScriptPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "DbGenScript.txt");
+
+        string? dbGenScriptPrev = null;
+        if (File.Exists(dbGenScriptPath))
+        {
+            dbGenScriptPrev = File.ReadAllText(dbGenScriptPath);
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
+        DbContext dbContext = ServiceProvider.GetService<DbContext>();
+        string dbGenScript = dbContext.GetDbGenScript();
+        File.WriteAllText(dbGenScriptPath, dbGenScript);
+
+        if (dbGenScriptPrev == null)
         {
-            Initialize();
-
-            Window window = new Window(new AppShell());
-            window.Title = "WonderWall";
-            window.MinimumWidth = 1200;
-            window.MinimumHeight = 900;
-
-            return window;
+            return;
         }
 
-        private void Initialize()
+        if (dbGenScriptPrev != dbGenScript)
         {
-            if (IsInitialized)
-                return;
-
-            IsInitialized = true;
-            InitializeTrayIcon();
+            MigrateDb(dbContext, dbGenScript);
         }
+    }
 
-        private void InitializeTrayIcon()
+    private void MigrateDb(DbContext dbContext, string dbGenScript)
+    {
+        List<string> keyDefinitions = [];
+
+        foreach (string tableDefinition in dbGenScript!.Split(';'))
         {
-            ITrayService trayService = ServiceProvider.GetService<ITrayService>();
+            if (!tableDefinition.Contains("CREATE "))
+                continue;
 
-            if (trayService != null)
+            string tableDef = MakeSqliteConform(tableDefinition[tableDefinition.IndexOf("CREATE ")..]) + ";";
+            string type = tableDef.Substring(7, 5);
+            string table = tableDef.Split(' ')[2];
+            string[] fieldDefinitions = tableDef.Split('\n');
+
+            switch (type)
             {
-                trayService.Initialize();
+                case "TABLE":
+                    if (!dbContext.TableExists(table))
+                    {
+                        _ = dbContext.Database.ExecuteSqlRaw(tableDef);
+                    }
+                    else
+                    {
+                        for (int i = 1; i < fieldDefinitions.Length - 1; i++)
+                        {
+                            string fieldDefinition = fieldDefinitions[i].Trim();
+                            string[] fieldDefinitionParts = fieldDefinition.Split(' ');
+
+                            if (fieldDefinition.EndsWith(','))
+                            {
+                                fieldDefinition = fieldDefinition[..^1];
+                            }
+
+                            if (fieldDefinitionParts[0] == "CONSTRAINT")
+                            {
+                                if (!dbContext.ConstraintExists(table, fieldDefinitionParts[1]))
+                                {
+                                    keyDefinitions.Add($"ALTER TABLE {table} ADD {fieldDefinition}");
+                                }
+                            }
+                            else
+                            {
+                                if (!dbContext.ColumnExists(table, fieldDefinitionParts[0]))
+                                {
+                                    _ = dbContext.Database.ExecuteSqlRaw($"ALTER TABLE {table} ADD {fieldDefinition}");
+                                }
+                            }
+                        }
+                    }
+                    break;
+
+                case "INDEX":
+                    keyDefinitions.Add(tableDef.Insert(13, "IF NOT EXISTS "));
+                    break;
             }
         }
+
+        foreach (string keyDefinition in keyDefinitions)
+        {
+            _ = dbContext.Database.ExecuteSqlRaw(keyDefinition);
+        }
+    }
+
+    private string MakeSqliteConform(string sql)
+    {
+        return sql
+            .Replace("\"", "")
+            .Replace("INTEGER NOT NULL", "INTEGER NOT NULL DEFAULT '0'")
+            .Replace("TEXT NOT NULL", "TEXT NOT NULL DEFAULT ''")
+            .Replace("REAL NOT NULL", "REAL NOT NULL DEFAULT '0.0'");
     }
 }
--- Database/DbContext.cs
+++ Database/DbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
 using WonderWall.Data;
 
 namespace WonderWall.Database;
@@ -19,4 +20,58 @@ public class DbContext : Microsoft.EntityFrameworkCore.DbContext
         string dbPath = Path.Combine(FileSystem.AppDataDirectory, "wonderwall.db");
         optionsBuilder.UseSqlite($"Filename={dbPath}");
     }
+
+    public string GetDbGenScript()
+    {
+        return Database.GenerateCreateScript().Trim();
+    }
+
+    public bool TableExists(string table)
+    {
+        using var command = Database.GetDbConnection().CreateCommand();
+        command.CommandText = $"SELECT name from sqlite_master WHERE type='table' and name='{table}'";
+        Database.OpenConnection();
+        using var result = command.ExecuteReader();
+        return result.Read();
+    }
+
+    public bool ColumnExists(string table, string column)
+    {
+        using var command = Database.GetDbConnection().CreateCommand();
+        command.CommandText = $"pragma table_info('{table}')";
+        Database.OpenConnection();
+        using var result = command.ExecuteReader();
+        while (result.Read())
+        {
+            if (result.GetString(1) == column)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool ConstraintExists(string table, string constraint)
+    {
+        bool resultBool;
+
+        using (DbCommand command = Database.GetDbConnection().CreateCommand())
+        {
+            command.CommandText = $"SELECT sql from sqlite_master WHERE type='table' and name='{table}'";
+            Database.OpenConnection();
+            using DbDataReader result = command.ExecuteReader();
+            if (result.Read())
+            {
+                string lstr_sql = result[0]?.ToString() ?? "";
+                resultBool = lstr_sql.Contains(constraint);
+            }
+            else
+            {
+                resultBool = false;
+            }
+        }
+
+        return resultBool;
+    }
+
 }
